6bdc14d2111d5ce2c69fec8fb59ad5c61c91cb6a
pakupaku <kof2464706@o-hara.stu.ac.jp>
2025-02-21 10:06:36 +0900

    プロジェクト ファイルを追加します。

---
+++ TeamProject/WinMain.cpp
@@ -0,0 +1,21 @@
+#include<Dxlib.h>
+
+static const int windowWidth = 640;  // ウィンドウのサイズ(DxLibのデフォルトサイズと同じ)
+static const int windowHeight = 480;
+static const int colorDepth = 32;    // 32bit カラーを使用する
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+    SetGraphMode(windowWidth, windowHeight, colorDepth);
+    ChangeWindowMode(TRUE);
+    if (DxLib_Init() == -1) return -1;
+
+    int a = 0;
+    while (a == 0) {
+        DrawFormatString(10, 25, GetColor(255, 255, 255), "HelloWorld!");
+        if (CheckHitKey(KEY_INPUT_A))    a++;
+    }
+    DxLib_End();
+    return 0;
+}
