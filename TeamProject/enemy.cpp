7231e190f462c6fe0c83e83e024b3d2db873acc1
usagi <kof2161705@stu.o-hara.ac.jp>
2025-02-28 13:52:16 +0900

    コメント整理

--- TeamProject/enemy.cpp
+++ TeamProject/enemy.cpp
@@ -1,38 +1,12 @@
 #include "DxLib.h"
 #include "enemy.h"
 
-/*static int m_image;//画像
-static int m_y;//y座標
-static int m_x;//x座標
-
-//初期化
-void Enemy_Initialize() {
-	m_image = LoadGraph("../aseets/Enemy1.png"); // 画像読み込み
-	m_x = 0;//座標
-	m_y = 0;
-}
-
-//動きの計算
-void Enemy_Update() {
-	//m_y++;
-	//m_x++;
-}
-
-//描画
-void Enemy_Draw() {
-	DrawExtendGraph(m_x, m_y, 40, 40, m_image, TRUE);
-	//DrawExtendGraph(m_x, m_y, m_x+40, m_y+40, m_image, TRUE);
-}
-
-//終了
-void Enemy_Finalize() {
-	DeleteGraph(m_image);
-}*/
 
 slime::slime(int x, int y) :m_x(x), m_y(y){
 	m_image = LoadGraph("../aseets/参考スライム.png"); // 画像読み込み
 }
 
+//メモリ開放のだったが動かない
 /*slime::~slime() {
 	DeleteGraph(m_image);
 }*/
@@ -54,6 +28,7 @@ void slime::SetPosition(int x, int y) {
 	m_y = y;
 }
 
+//終了時のメモリ開放
 void slime::Enemy_Finalize() {
 	DeleteGraph(m_image);
 }
--- TeamProject/enemy.h
+++ TeamProject/enemy.h
@@ -4,20 +4,19 @@
 
 class slime {
 public:
-	slime(int x,int y);
+	slime(int x,int y);//初期化
 
-	void Enemy_Update();
-	void Enemy_Draw();
+	void Enemy_Update();//移動
+	void Enemy_Draw();//描画
 
-	void SetPosition(int x, int y);
-	void Enemy_Finalize();
+	void SetPosition(int x, int y);//ポジション設定
+	void Enemy_Finalize();//データ開放
 
+	//動かしたくないもの
 private:
 	int m_image;
 	int m_y, m_x;
 };
-
-
 
 #endif
 
