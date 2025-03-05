2092f4273c8996330988b7150a5c2c050477d7be
student <kof2464746@stu.o-hara.ac.jp>
2025-03-05 13:46:59 +0900

    Mainで動くようにできた

---
+++ TeamProject/Main.cpp
@@ -0,0 +1,37 @@
+#include "DxLib.h"
+#include "Map.h"
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+    ChangeWindowMode(TRUE); // ウィンドウモードに変更
+    SetGraphMode(500, 400, 32); // 解像度とカラー設定
+
+    if (DxLib_Init() == -1)
+    {
+        return -1; // 初期化失敗の場合、終了
+    }
+
+    // Mapインスタンスの作成
+    Map map;
+    map.LoadGraphics();  // グラフィックの読み込み
+
+    SetDrawScreen(DX_SCREEN_BACK);
+
+    while (1)
+    {
+        ClearDrawScreen();
+
+        if (CheckHitKey(KEY_INPUT_ESCAPE)) { // ESCキーが押された場合
+            break; // ループを抜けて終了処理へ
+        }
+
+        map.DrawMap();  // マップを描画
+
+        ScreenFlip();  // バックバッファを画面に反映
+    }
+
+    map.DeleteGraphics();  // グラフィックの解放
+    DxLib_End();
+    return 0;
+}
--- TeamProject/Map.h
+++ TeamProject/Map.h
@@ -1,5 +1,33 @@
 
+#ifndef MAP_H
+#define MAP_H
+
 #include "DxLib.h"
 
+struct Map {
+    static const int mapWidth = 10;    // マップの横のサイズ
+    static const int mapHeight = 8;    // マップの縦のサイズ
+    static const int MapChipsize = 64; // マップチップのサイズ（仮置き）
 
-void DrawMap();
+    // マップデータ（0: 床, 1: 壁, 2: 壁, 3: ドアなど）
+    int mapData[mapHeight][mapWidth];
+
+    // グラフィックデータ
+    int imgFloor;
+    int imgWall;
+    int imgPlayer;
+
+    // コンストラクタ
+    Map();
+
+    // マップを描画する関数
+    void DrawMap() const;
+
+    // グラフィックのロード
+    void LoadGraphics();
+
+    // グラフィックの解放
+    void DeleteGraphics();
+};
+
+#endif // MAP_H
