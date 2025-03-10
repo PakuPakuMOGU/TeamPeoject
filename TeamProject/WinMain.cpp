0206f2f774924c0e6808a1803e815d5468103c84
usagi <kof2161705@stu.o-hara.ac.jp>
2025-03-10 16:40:53 +0900

    敵の移動バグ（修正中）

--- TeamProject/WinMain.cpp
+++ TeamProject/WinMain.cpp
@@ -4,34 +4,13 @@
 
 static const int colorDepth = 32;    // カラー
 
-const int NUM_SLIMES = 2;  // スライムの数
-const int ENEMY = 1;
-int maxX = 100;
-int minX = 0;
-
-slime slimeArray[NUM_SLIMES] = {
-        slime(0, 0,minX,maxX),  // スライムの数だけ初期化
-        slime(50, 50,minX,maxX)
-};
-
-goes goesArray[ENEMY] = {
-        goes(30,30)
-};
-
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {  
     ChangeWindowMode(TRUE);
     if (DxLib_Init() == -1) return -1;
 
-
     System_Main();
-
-    slime slimeArray[NUM_SLIMES] = {
-        slime(0, 0,minX,maxX),  // 初期位置を指定してスライムを作成
-        slime(50, 50,minX,maxX)
-    };
-
 
     DxLib_End();
     return 0;
